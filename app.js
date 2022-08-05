07e8f7e7a3fe3b5ebf24322e3e03a6303a759bbd
Fabian Soriano <fabo.soriano@gmail.com>
2022-08-04 19:33:12 -0700

    class activity

--- app.js
+++ app.js
@@ -1,15 +1,43 @@
 class Casino {
-    // Write code here
-};
-
-// TESTS
-const myCasino = new Casino("HackerU Casino");
-console.log(myCasino);
-myCasino.playGame(5);
-myCasino.playGame(15);
-myCasino.playGame(25);
-myCasino.playGame(35);
-
+    // constructor to build class
+      constructor(name, fakeCoin){
+        this.name = name;
+        this.timesPlayed = 0;
+        this.winnings = 0;
+        this.fakeCoin = fakeCoin;
+      }
+    
+    playGame(betAmount){
+      // increase times played by 1 every time we play
+      this.timesPlayed += 1
+      // we are determining if Player Wins (else) or Loses (if)
+      // casino wins if number is less than .5 OR fake coin is true
+      if(Math.random() <= .5 || this.fakeCoin){
+        console.log(this.name + " wins!")
+        this.timesPlayed = 0;
+      } 
+      // otherwise player wins 
+      else {
+        console.log("You win " + betAmount)
+        this.winnings = betAmount * (this.timesPlayed + 1)
+        this.timesPlayed += 1;
+      }
+    }
+  
+  
+    
+  };
+  
+  // TESTS
+  // creating the new casino object
+  const myCasino = new Casino("HackerU Casino", true);
+  console.log(myCasino);
+  // playing the game -- passing in the bet amount to playGame
+  myCasino.playGame(5);
+  myCasino.playGame(15);
+  myCasino.playGame(25);
+  myCasino.playGame(35);
+  
 // BONUS TESTS
 /*
 const myBonusCasino = new Casino("HackerU Bonus Casino", true);
